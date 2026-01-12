dd441b807893e68f732c54477c5d920d221ce392
Archie Gledhill <ag1@aargs.local>
2026-01-12 10:24:15 +0000

    spotify auth flow

--- src/curatio/go.mod
+++ src/curatio/go.mod
@@ -11,9 +11,11 @@ require (
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/zmb3/spotify/v2 v2.4.3 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genai v1.36.0 // indirect
--- src/curatio/main.go
+++ src/curatio/main.go
@@ -1,42 +1,68 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"strings"
+	"net/http"
 
-	"google.golang.org/genai"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
 )
 
-// Initialise gemini
+// TLS config
+const keyFile = "/Users/ag1/Documents/curatio_httpscert/server.key"
+const certFile = "/Users/ag1/Documents/curatio_httpscert/cert.conf"
+
+// Redirect URI for spotify API
+const redirectURI = "https://localhost:8888/callback/"
+
+var (
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
 func main() {
-	os.Getenv("GEMINI_API_KEY")
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
 
-	ctx := context.Background()
-	client, err := genai.NewClient(ctx, nil)
+	go func() {
+		err := http.ListenAndServeTLS(":8888", certFile, keyFile, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	url := auth.AuthURL(state)
+	fmt.Println("Please log into spotify by visiting the following page:", url)
+
+	//Wait for auth to complete
+	client := <-ch
+
+	//Use the client to make calls that require auth
+	user, err := client.CurrentUser(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("You are now logged in as:", user.ID)
+}
 
-	// Ask User for a message
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Whaddup? ...")
-	userInput, _ := reader.ReadString('\n')
-	userInput = strings.TrimSpace(userInput)
-
-	// Query model
-	resp, err := client.Models.GenerateContent(
-		ctx,
-		"gemini-2.5-flash",
-		genai.Text(userInput),
-		nil,
-	)
-
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
 	if err != nil {
+		http.Error(w, "couldnt get token", http.StatusForbidden)
 		log.Fatal(err)
 	}
-	fmt.Println(resp.Text())
+
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	//Use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
 }
